2ac6502346271a890daad50de7b850ed2f31a041
Jacob Vibert <jacob.vibert@sky.uk>
2022-05-27 22:20:04 +0100

    Add image upload

---
+++ Models/ViewModels/EventDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CO567WBL_Ticket_App.Models.ViewModels
+{
+    public class EventDetailViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateOfMovie { get; set; }
+        public string EventPicture { get; set; }
+    }
+}
